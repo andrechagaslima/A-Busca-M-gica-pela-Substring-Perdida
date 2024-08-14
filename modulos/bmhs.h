9520322b18d4fe2fd9ebcae2b78518470a1bffdf
andrechagaslima <andrechalima@gmail.com>
2024-08-13 23:36:32 -0300

    BMHS

---
+++ modulos/bmhs.h
@@ -0,0 +1,10 @@
+#ifndef BMHS_H
+#define BMHS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool BMHS(char *text, int n, char *pattern, int m);
+
+#endif
---
+++ principal/bmhs.c
@@ -0,0 +1,32 @@
+#include "bmhs.h"
+#define MaxChar 256
+
+bool BMHS(char *text, int n, char *pattern, int m){
+
+     int i, j, k;
+     int d[MaxChar];
+
+     for(j = 0; j < MaxChar; j++){
+          d[j] = (m + 1);
+     }
+
+     for(j = 1; j < m; j++){
+          d[(int)pattern[j]] = (m - j);
+     }
+         
+     i = m;
+
+     while(i <= n){
+
+          k = i - 1;
+          j = m - 1;
+          while((text[k] == pattern[j]) && (j >= 0)){
+               k--;
+               j--;
+          }
+          if(j < 0){
+               printf("Casou: %d\n", (k+1));
+          }
+          i = i + d[(int)text[i]];
+     }
+}
